ef0366fb37b092271d43031177a26d47f204d588
Zayah117 <isaiah.baker@simpledove.com>
2016-08-11 14:27:22 -0400

    Location names display in sidebar

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
+// Declare map and location variables
+
 var map;
+var locations = [
+	{name: "Mulberry Health and Retirement", location: {lat: 40.348114, lng: -86.67255899999999}},
+	{name: "Pizza King", location: {lat: 40.343967, lng: -86.6661139}},
+	{name: "Southfork", location: {lat: 40.344499, lng: -86.665989}},
+	{name: "Mulberry Park", location: {lat: 40.340536, lng: -86.659653}},
+	{name: "Mulberry Community Library", location: {lat: 40.344274, lng: -86.657544}},
+	{name: "Bodline Funeral Home", location: {lat: 40.346559, lng: -86.666670}},
+	{name: "Marathon Gas", location: {lat: 40.344854, lng: -86.664828}},
+	{name: "Farmer's Bank", location: {lat: 40.344419, lng: -86.665319}},
+	{name: "Mulberry Telephone", location: {lat: 40.344214, lng: -86.666254}}
+];
+
+// Initialize map
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
+		// Centers to Mulberry IN
 		center: {lat: 40.3453136, lng: -86.6668564},
 		zoom: 16
 	});
+	// Create marker list
 	var markers = [];
 
-	var locations = [
-		{name: "Mulberry Health and Retirement", location: {lat: 40.348114, lng: -86.67255899999999}},
-		{name: "Pizza King", location: {lat: 40.343967, lng: -86.6661139}},
-		{name: "Southfork", location: {lat: 40.344499, lng: -86.665989}},
-		{name: "Mulberry Park", location: {lat: 40.340536, lng: -86.659653}},
-		{name: "Mulberry Community Library", location: {lat: 40.344274, lng: -86.657544}},
-		{name: "Bodline Funeral Home", location: {lat: 40.346559, lng: -86.666670}},
-		{name: "Marathon Gas", location: {lat: 40.344854, lng: -86.664828}},
-		{name: "Farmer's Bank", location: {lat: 40.344419, lng: -86.665319}},
-		{name: "Mulberry Telephone", location: {lat: 40.344214, lng: -86.666254}}
-	];
+	// Add markers to this list taking data from locations
 	for (i in locations) {
 		current_location = locations[i];
 		var marker = new google.maps.Marker({
@@ -29,3 +36,24 @@ function initMap() {
 		markers.push(marker);
 	}
 }
+
+// Data for locations
+var Location = function(data) {
+	this.name = ko.observable(data.name);
+	this.location = ko.observable(data.location);
+}
+
+// My ViewModel
+var ViewModel = function() {
+	var self = this;
+
+	this.locationList = ko.observableArray([]);
+
+	locations.forEach(function(locationItem){
+		self.locationList.push(new Location(locationItem));
+	});
+	console.log(this.locationList())
+
+}
+
+ko.applyBindings(new ViewModel);
