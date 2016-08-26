7d131f3da3e3f667f7b547f49f4f277e66681184
Zayah117 <isaiah.baker@simpledove.com>
2016-08-26 11:04:16 -0400

    Convert indention to tabs

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
 var map;
 var locations = [
 	{name: "Mulberry Health and Retirement", location: {lat: 40.348114, lng: -86.67255899999999}},
-	{name: "Pizza King", location: {lat: 40.343967, lng: -86.6661139}},
-	{name: "Southfork", location: {lat: 40.344499, lng: -86.665989}},
+	{name: "Pizza King of Mulberry", location: {lat: 40.343967, lng: -86.6661139}},
+	{name: "Southfork Restaurant & Pub", location: {lat: 40.344499, lng: -86.665989}},
 	{name: "Mulberry Park", location: {lat: 40.340536, lng: -86.659653}},
 	{name: "Mulberry Community Library", location: {lat: 40.344274, lng: -86.657544}},
-	{name: "Bodline Funeral Home", location: {lat: 40.346559, lng: -86.666670}},
+	{name: "Genda Funeral Home-Mulberry Chapel", location: {lat: 40.346559, lng: -86.666670}},
 	{name: "Marathon Gas", location: {lat: 40.344854, lng: -86.664828}},
 	{name: "Farmer's Bank", location: {lat: 40.344419, lng: -86.665319}},
 	{name: "Mulberry Telephone", location: {lat: 40.344214, lng: -86.666254}}
@@ -67,50 +67,11 @@ function initMap() {
 		currentLocation.marker = marker;
 		currentLocation.infoWindow = infoWindow;
 	}
+	var myReviews = getData();
 	ko.applyBindings(new ViewModel);
-	getData();
 }
 
-// MY OLD CODE
-
-/*
-function getData() {
-	var yelpUrl = 'https://api.yelp.com/v2/search?location=San+Francisco';
-
-	var parameters = {
-		oauth_consumer_key: 'jcxihH9bOq3E-J4DBbcKFA',
-		oauth_token: '97hDLyug92HlKhfUUholXDEr6C4ztNqd',
-		oauth_signature_method: 'hmac-sha1',
-		oauth_version: '2.0',
-		oauth_nonce: (Math.floor(Math.random() * 1e12).toString()),
-		oauth_timestamp: Math.floor(Date.now()/1000),
-		callback: 'cb'
-	};
-
-	// var encodedSignature = oauthSignature.generate('GET',yelpUrl, parameters, 'SxDknbz-0-uyzrhyvyvV6mralX4', 'kLwD7MBstt1lq3zDqkjXkL2GRUo');
-
-	$.ajax({
-		url: yelpUrl,
-		data: parameters,
-		cache: true,
-		dataType: 'jsonp',
-		success: function(results) {
-			console.log(results);
-		},
-		fail: function() {
-			console.log('FAIL');
-		}
-
-
-	});
-
-	timestamp = (Date.now()/100);
-	$.ajax('https://api.yelp.com/v2/search?term=food&location=San+Francisco&oauth_consumer_key=jcxihH9bOq3E-J4DBbcKFA&oauth_token=97hDLyug92HlKhfUUholXDEr6C4ztNqd&oauth_signature_method=hmac-sha1&oauth_signature=kLwD7MBstt1lq3zDqkjXkL2GRUo&oauth_timestamp=' + timestamp + '')
-}
-
-*/
-
-// MarkN's code: https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/4
+// Inspired by MarkN's code: https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/4
 
 function getData() {
 	/**
@@ -118,7 +79,7 @@ function getData() {
 	 * @return {string}
 	 */
 	function nonce_generate() {
-	  return (Math.floor(Math.random() * 1e12).toString());
+		return (Math.floor(Math.random() * 1e12).toString());
 	}
 
 	// var YELP_BASE_URL = 'https://api.yelp.com/';
@@ -131,41 +92,48 @@ function getData() {
 
 	// var yelp_url = 'https://api.yelp.com/v2/search?term=food&location=San+Francisco';
 
-	    var parameters = {
-	      oauth_consumer_key: MY_KEY,
-	      oauth_token: TOKEN,
-	      oauth_nonce: nonce_generate(),
-	      oauth_timestamp: Math.floor(Date.now()/1000),
-	      oauth_signature_method: 'HMAC-SHA1',
-	      oauth_version : '1.0',
-	      limit: 10,
-	      location: 'San Francisco',
-	      term: 'food',
-	      callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-	    };
+		var parameters = {
+			oauth_consumer_key: MY_KEY,
+			oauth_token: TOKEN,
+			oauth_nonce: nonce_generate(),
+			oauth_timestamp: Math.floor(Date.now()/1000),
+			oauth_signature_method: 'HMAC-SHA1',
+			oauth_version : '1.0',
+			limit: 10,
+			location: 'Mulberry IN',
+			// term: 'food',
+			callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+		};
 
-	    var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, SECRET_KEY, SECRET_TOKEN);
-	    parameters.oauth_signature = encodedSignature;
+		var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, SECRET_KEY, SECRET_TOKEN);
+		parameters.oauth_signature = encodedSignature;
 
-	    var settings = {
-	      url: yelp_url,
-	      data: parameters,
-	      cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-	      dataType: 'jsonp',
-	      success: function(results) {
-	        // Do stuff with results
-	        console.log('success!');
-	        console.log(results);
-	      },
-	      error: function(e) {
-	        // Do stuff on fail
-	        console.log('error!');
-	        console.log(e);
-	      }
-	    };
+		var settings = {
+			url: yelp_url,
+			data: parameters,
+			cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+			dataType: 'jsonp',
+			success: function(results) {
+				var reviews = [];
+				// Do stuff with results
+				console.log('success!');
+				console.log(results);
+				for (i in results.businesses) {
+					if (results.businesses[i].location.city === "Mulberry") {
+						reviews.push(results.businesses[i]);
+					}
+				}
+				return reviews;
+			},
+			error: function(e) {
+				// Do stuff on fail
+				console.log('error!');
+				console.log(e);
+			}
+		};
 
-	    // Send AJAX query via jQuery library.
-	    $.ajax(settings);
+		// Send AJAX query via jQuery library.
+		$.ajax(settings);
 }
 
 // Data for locations
@@ -184,9 +152,6 @@ var Location = function(data) {
 // My ViewModel
 var ViewModel = function() {
 	var self = this;
-
-	// Pass my array of markers into the view model
-	// this.markerList = ko.observableArray(markers);
 
 	// Links to the filter input
 	this.filterValue = ko.observable("");
